8b53d57566b4293f57ec0083dfd1488905169e1e
Hoan Do <vanhoan310@gmail.com>
2024-03-19 10:54:22 +0100

    Add reproducibility

--- reproducibility/DNAKmer/runKounta.sh
+++ reproducibility/DNAKmer/runKounta.sh
@@ -6,9 +6,3 @@ kounta --kmer 31 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk31F
 kounta --kmer 31 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk31Fold3.tsv /data/hoan/amromics/prediction/data/Ecoli1936/prokkaMore/Fold3/*.fna
 
 kounta --kmer 31 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk31Fold4.tsv /data/hoan/amromics/prediction/data/Ecoli1936/prokkaMore/Fold4/*.fna
-
-
-kounta --kmer 20 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk20Fold1.tsv /data/hoan/amromics/prediction/data/Ecoli1936/prokkaMore/Fold1/*.fna
-kounta --kmer 20 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk20Fold2.tsv /data/hoan/amromics/prediction/data/Ecoli1936/prokkaMore/Fold2/*.fna
-kounta --kmer 20 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk20Fold3.tsv /data/hoan/amromics/prediction/data/Ecoli1936/prokkaMore/Fold3/*.fna
-kounta --kmer 20 --out /data/hoan/amromics/prediction/data/Ecoli1936_DNAKmerk20Fold4.tsv /data/hoan/amromics/prediction/data/Ecoli1936/prokkaMore/Fold4/*.fna
